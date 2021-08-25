95fb262525ec3043de0aa93280fd3d6f39b6681e
ahriri <katielo522@gmail.com>
2021-08-24 20:39:36 -0700

    added cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
-
+var cors = require('cors');
+app.use(cors());
 var metadataRouter = require('./routes/api/token-metadata')
 app.use('/api/token-metadata', metadataRouter);
 
